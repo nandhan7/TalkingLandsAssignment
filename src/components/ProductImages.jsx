d657dea5e8b3277ebabfbe51ddc41e9c4d42ed7f
nandhan7 <89990618+nandhan7@users.noreply.github.com>
2024-09-14 14:37:41 +0530

    Update ProductImages.jsx

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -4,7 +4,7 @@ export default function ProductImages() {
   return (
     <div className="md:w-1/2 p-4">
       <img
-        src="./pexels-olly-972804.jpg"
+        src="/pexels-olly-972804.jpg"
         alt="Product"
         className="w-full h-auto rounded-lg mb-4"
       />
